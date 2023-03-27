296b87dc80298c168c8b0e5e08e580dcdf4d015c
Dan Fabulich <dan@fabulich.com>
2023-03-27 12:41:26 -0700

    Remove deb/rpm builders, use linux zip builder

--- forge.config.js
+++ forge.config.js
@@ -8,15 +8,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      platforms: ['linux'],
     },
   ],
 };
